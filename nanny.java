b695f409b7d694712734e52ead6792519e043f9d
Aaron Barack <abarack@gmail.com>
2018-10-29 10:14:21 -0700

    Added base file: ready to work

---
+++ nanny.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+
+class nanny{
+  public static void main(Strings[] args){
+    //Get the input from the user
+    //
+    System.out.println("1. Get a nanny\n 2. Get baby products\n");
+    int input = System.
+  }
+}
